99f02a2dd91509bb161ad37fee1076993e938032
fierro-cristina <jazmin.fierro@senirop.com>
2022-12-29 21:32:57 -0500

    Pacman game in vanilla javascript

---
+++ app/Methods/ghostMoves.js
@@ -0,0 +1,16 @@
+import { DIRECTIONS, OBJECT_TYPE } from "../Setups/setup";
+
+export function randomMovement(position, direction, objectExists) {
+  let dir = direction;
+  let nextMovePos = position + dir.movement;
+  const keys = Object.keys(DIRECTIONS);
+  while (
+    objectExists(nextMovePos, OBJECT_TYPE.WALL) ||
+    objectExists(nextMovePos, OBJECT_TYPE.GHOST)
+  ) {
+    const key = keys[Math.floor(Math.random() * keys.length)];
+    dir = DIRECTIONS[key];
+    nextMovePos = position + dir.movement;
+  }
+  return { nextMovePos, direction: dir };
+}
